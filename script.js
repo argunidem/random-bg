45469e7d6c000d77c2c171bea621fd2f827df30f
argunidem <argun.idem@hotmail.com>
2023-03-21 19:32:39 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const changeColor = () => {
+  document.body.style.backgroundColor =
+    'rgba(' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    Math.round(Math.random() * 255) +
+    ')';
+};
